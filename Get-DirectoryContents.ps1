dc7de7b9b85fcace81e5effbe2962769db42f7cd
Zealous <65210276+heinzrc@users.noreply.github.com>
2023-02-13 10:01:48 -0600

    Code Rewrite
    Various Fixes

--- Get-DirectoryContents.ps1
+++ Get-DirectoryContents.ps1
@@ -1,114 +1,90 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [string]$SrcPath
+    [string]$SourcePath
 )
 
 #Default 7-Zip install path
 $7zipPath = "C:\Program Files\7-Zip\7z.exe"
-Set-Alias sz $7zipPath
+Set-Alias 7zip $7zipPath
 
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 $CSVPath = "$($DesktopPath)\Output.csv"
 
 #Check if TempDir already exists and delete if so
-$TempPath = "$($DesktopPath)\TempDir"
-if (Test-Path $TempPath) {
-    Remove-Item "$($DesktopPath)\TempDir" -Recurse -Force
+$TempDirectory = "$($DesktopPath)\TempDir"
+if (Test-Path $TempDirectory) {
+    Remove-Item $TempDirectory -Recurse -Force
 }
-New-Item -Path $TempPath -ItemType Directory
+New-Item -Path $TempDirectory -ItemType Directory
 
 #Create list of PSObjects for output
 $Output = [System.Collections.Generic.List[PSOBJECT]]::new()
 
-function SetOutput {
-    param (
-        $Name
-    )
+function SetOutput ($Name) {
     $Output.Add([PSCustomObject]@{
             'Filename' = $Name
         })
 }
 
-function GetLeaf {
-    param (
-        $Str
-    )
-    $Leaf = Split-Path $Str -Leaf
+function GetLeaf ($Path){
+    $Leaf = Split-Path $Path -Leaf
     $LeafArr = $Leaf.Split(".")
     $LeafArr[0]
 }
 
 #Start Recursive Function
-function GetDirContents {
-    param (
-        $DirPath
-    )
-    "Test: $DirPath"
+function GetDirContents ($DirPath){
     $files = Get-ChildItem -Path $DirPath -Recurse -Force
 
     foreach ($file in $files) {
-        
-        $FileShortName = $File.Fullname.Replace($DirPath, "")
-        $FileShortName = $FileShortName -Split '\.', 2 #Accounts for files with multiple file extensions
-        $ArchivePath = "$($DirPath)$($FileShortName[0]).$($FileShortName[1])"
-        $FileShortName[0]
-        if (Test-Path $ArchivePath) {
-            if ($file.Extension -like "*.zip") {
-                Expand-Archive -Path $ArchivePath -DestinationPath $TempPath -Force
-                SetOutput -Name $File.name
+
+        if ($file.Extension -like "*.zip") {
+            Expand-Archive -Path $File.FullName -DestinationPath $TempDirectory -Force
+            SetOutput -Name $File.name
     
-                GetDirContents -DirPath "$($TempPath)$($FileShortName[0])"
+            GetDirContents -DirPath "$($TempDirectory)\$($File.BaseName)"
     
-            }
-            elseif ($file.Extension -like "*.7z") {
-                sz x -y -o"$TempPath" $ArchivePath 
-                SetOutput -Name $File.name
+        }
+        elseif ($file.Extension -like "*.7z") {
+            7zip x -y -o"$TempDirectory" $File.FullName
+            SetOutput -Name $File.name
                 
-                GetDirContents -DirPath "$($TempPath)$($FileShortName[0])"
+            GetDirContents -DirPath "$($TempDirectory)\$($File.BaseName)"
     
-            }
-            elseif ($file.Attributes -eq "Directory") {
-    
-                SetOutput -Name $File.name
-    
-                GetDirContents -DirPath "$($DirPath)$($FileShortName[0])"
-    
-            }
-            else {
-    
-                SetOutput -Name $File.name
-            }
-        } 
+        }
+        else {
+            SetOutput -Name $File.name
+        }
+        
     } 
 }
 #End Recursive Function
 
 #Start Input Type Check
-if ($SrcPath -like "*.zip") {
-    Expand-Archive -Path $SrcPath -DestinationPath $TempPath 
-    if (Test-Path "$TempPath\$(GetLeaf -Str $SrcPath)") {
-        GetDirContents -DirPath "$TempPath\$(GetLeaf -Str $SrcPath)"
+if ($SourcePath -like "*.zip") {
+    Expand-Archive -Path $SourcePath -DestinationPath $TempDirectory 
+    if (Test-Path "$TempDirectory\$(GetLeaf -Path $SourcePath)") {
+        GetDirContents -DirPath "$TempDirectory\$(GetLeaf -Path $SourcePath)"
     }
     else {
-        GetDirContents -DirPath "$TempPath"
+        GetDirContents -DirPath "$TempDirectory"
     }
 
 }
-elseif ($SrcPath -like "*.7z") {
-    sz x -y -o"$TempPath" "$SrcPath"
-    if (Test-Path "$TempPath\$(GetLeaf -Str $SrcPath)") {
-        GetDirContents -DirPath "$TempPath\$(GetLeaf -Str $SrcPath)"
+elseif ($SourcePath -like "*.7z") {
+    7zip x -y -o"$TempDirectory" "$SourcePath"
+    if (Test-Path "$TempDirectory\$(GetLeaf -Path $SourcePath)") {
+        GetDirContents -DirPath "$TempDirectory\$(GetLeaf -Path $SourcePath)"
     }
     else {
-        GetDirContents -DirPath "$TempPath"
+        GetDirContents -DirPath "$TempDirectory"
     }
 
 }
 else {
     #Just a folder
-
-    GetDirContents -DirPath $SrcPath
+    GetDirContents -DirPath $SourcePath
 }
 #End Input Type Check
 
